5916bec070610414caa5663c9d199b01e39bd30d
Blandine Zongo <blandinebess7@gmail.com>
2025-07-08 21:26:42 -0400

    https://github.com/Blandinebess/pet-crud-app.git

---
+++ src/components/dynamo.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({
+  region: process.env.REACT_APP_AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+  },
+});
+const docClient = DynamoDBDocumentClient.from(client);
+
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+import path from "node:path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import Dotenv from "dotenv-webpack";
+import webpack from "webpack";
+
+export default (_env, argv) => {
+  const prod = argv.mode === "production";
+
+  return {
+    entry: "./src/index.jsx",
+    output: {
+      filename: prod ? "bundle.[contenthash].js" : "bundle.js",
+      path: path.resolve("dist"),
+      publicPath: "/",
+      clean: true,
+    },
+    resolve: { extensions: [".js", ".jsx"] },
+    devtool: prod ? "source-map" : "eval-cheap-module-source-map",
+    devServer: {
+      hot: true,
+      historyApiFallback: true,
+      port: 3000,
+    },
+    module: {
+      rules: [
+        // JavaScript and JSX
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              cacheDirectory: true,
+              plugins: [!prod && "react-refresh/babel"].filter(Boolean),
+            },
+          },
+        },
+        // Plain CSS
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        // SCSS (optional later)
+        {
+          test: /\.scss$/i,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        // Images and assets
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: "asset",
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "public/index.html" }),
+      !prod && new ReactRefreshWebpackPlugin(),
+      !prod && new Dotenv(),
+      prod &&
+        new webpack.DefinePlugin({
+          "process.env.REACT_APP_AWS_REGION": JSON.stringify(
+            process.env.REACT_APP_AWS_REGION
+          ),
+        }),
+    ].filter(Boolean),
+  };
+};
